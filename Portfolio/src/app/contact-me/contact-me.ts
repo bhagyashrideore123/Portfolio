1d6848f24159b8890991a239df0da750f4084a5f
deore.bhagyashri28 <deore.bhagyashri28@gmail.com>
2026-09-24 11:18:46 +0200

    feat: developed logic of contact form.

--- Portfolio/src/app/contact-me/contact-me.ts
+++ Portfolio/src/app/contact-me/contact-me.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,14 +9,35 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './contact-me.html',
 })
 export class ContactMe {
-  userForm = new FormGroup({
-    name: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)],
-    }),
-    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
-    message: new FormControl('', {
-      validators: [Validators.required, Validators.email, Validators.maxLength(100)],
-    }),
-    policycheck: new FormControl('', { validators: [Validators.required] }),
-  });
+  fb = inject(FormBuilder)
+  userForm = this.fb.group({
+      name:['',Validators.required, Validators.minLength(5)],
+      email:['',Validators.required, Validators.email] ,
+      message:['',Validators.required, Validators.email, Validators.maxLength(200)],
+      policycheck:['',Validators.required],
+  })
+  // userForm = new FormGroup({                                           //using form group
+  //   name: new FormControl('', {
+  //     validators: [Validators.required, Validators.minLength(5), Validators.maxLength(20)],
+  //   }),
+  //   email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+  //   message: new FormControl('', {
+  //     validators: [Validators.required, Validators.email, Validators.maxLength(100)],
+  //   }),
+  //   policycheck: new FormControl('', { validators: [Validators.required] }),
+  // });
+
+  formSubmit()
+  {
+    console.log(this.userForm.value);
+  }
+
+  formReset()
+  {
+    this.userForm.reset();
+  }
+
+  get email(){
+    return this.userForm.get("email")
+  }
 }
