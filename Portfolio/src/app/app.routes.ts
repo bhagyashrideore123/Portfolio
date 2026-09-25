1aee7bc3123b7a05d06de33b70045d5bedef8de8
deore.bhagyashri28 <deore.bhagyashri28@gmail.com>
2026-09-25 12:46:30 +0200

    feat:added stylesheets ,fonts also legal notice compoenent, hero componenet along with some styling.

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { NotFound } from './not-found/not-found';
+import { LegalNotice } from './legal-notice/legal-notice';
 
 export const routes: Routes = [
     {
         path:"",
         component:Home
+    },
+    {
+        path:"legal-notice",
+        component:LegalNotice
     },
     {
         path:"**",
---
+++ Portfolio/src/app/legal-notice/legal-notice.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-legal-notice',
+  styleUrl: './legal-notice.scss',
+  templateUrl: './legal-notice.html',
+})
+export class LegalNotice {}
